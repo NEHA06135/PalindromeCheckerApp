3b36b5f9bffbbd6a59cb2064ac12c7645ae4372b
NEHA06135 <nehajammula2006@gmail.com>
2026-02-20 09:56:16 +0530

    Initial commit - UC1 Palindrome App

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,26 @@
+/**
+ * ============================================================
+ * Use Case 1 - Application Entry & Welcome Message
+ * ============================================================
+ *
+ * This program displays the application name and version
+ * when the program starts.
+ *
+ * No palindrome logic is implemented in this use case.
+ *
+ * @version 1.0
+ */
+
+public class UseCase1PalindromeCheckerApp {
+
+    // Main method - Entry point of the program
+    public static void main(String[] args) {
+
+        System.out.println("==============================================");
+        System.out.println(" Welcome to the Palindrome Checker App ");
+        System.out.println(" Version : 1.0 ");
+        System.out.println(" System initialized successfully.");
+        System.out.println("==============================================");
+
+    }
+}
